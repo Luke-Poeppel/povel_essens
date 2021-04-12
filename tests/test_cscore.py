74c79c496ff670b83156f86267f1bc4dbf491efd
Luke Poeppel <luke.poeppel@gmail.com>
2021-04-12 18:38:27 -0400

    Added test for transform_to_time_scale.

---
+++ tests/test_cscore.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+
+from povel_essens.cscore import transform_to_time_scale
+
+def test_time_scale():
+	transformed_1 = transform_to_time_scale(temporal_pattern=np.array([0.5, 0.5, 0.375, 0.375]))
+	assert list(transformed_1) == [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0]
+
+	transformed_2 = transform_to_time_scale(np.array([1.0, 0.25, 0.25, 0.375]))
+	assert list(transformed_2) == [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0]
