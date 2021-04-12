bec7a98b087b21797d8ad50573084f23a3d4b4ea
Luke Poeppel <luke.poeppel@gmail.com>
2021-04-12 19:02:42 -0400

    Added pytest for Udikshana issue.

--- tests/test_cscore.py
+++ tests/test_cscore.py
@@ -9,3 +9,7 @@ def test_time_scale():
 
 	transformed_2 = transform_to_time_scale(np.array([1.0, 0.25, 0.25, 0.375]))
 	assert list(transformed_2) == [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0]
+
+	udikshana = np.array([0.5, 1.5, 0.5, 0.5, 1.0])
+	transformed_3 = transform_to_time_scale(udikshana)
+	assert list(transformed_3) == [1, 1, 0, 0, 1, 1, 1, 0]
